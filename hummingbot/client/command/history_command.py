40f93f10036830bab5d9803d3cbd79d7dc6b397a
Asman Malikov <asmanmalikov@gmail.com>
2025-02-10 12:04:22 +0200

    fix

--- hummingbot/client/command/history_command.py
+++ hummingbot/client/command/history_command.py
@@ -274,9 +274,7 @@ class HistoryCommand:
             if not trades:
                 return json.dumps({"error": "No past trades to report."}), []
 
-            loop = asyncio.get_event_loop()
-
-            return loop.run_until_complete(self.history_full_report(start_time, trades, precision, verbose, True)), list([TradeFill.to_bounty_api_json(t) for t in trades])
+            return asyncio.run(self.history_full_report(start_time, trades, precision, verbose, True)), list([TradeFill.to_bounty_api_json(t) for t in trades])
 
     async def history_full_report(self,  # type: HummingbotApplication
                              start_time: float,
